c3c584792ec023f3d62d80c2a341069a54d4aa19
khurramjn <39000712+khurramjn@users.noreply.github.com>
2019-07-31 10:56:58 -0400

    Create TelnetUsingNetmiko.py
    A simple script to Telnet using Netmiko.

---
+++ TelnetUsingNetmiko.py
@@ -0,0 +1,11 @@
+# Python 3
+# print(netmiko.__version__)
+# 2.3.3
+
+from netmiko import ConnectHandler
+
+device = ConnectHandler(ip='10.0.8.18', username='admin', password='Your-Password', device_type='cisco_ios_telnet')
+
+print(device.find_prompt())
+print(device.send_command('sh ip int bri'))
+device.disconnect()
