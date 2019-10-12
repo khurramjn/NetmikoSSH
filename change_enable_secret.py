c85d96fe29f9047f88742a5175dd3a6110883a41
khurramjn <39000712+khurramjn@users.noreply.github.com>
2019-10-11 20:36:40 -0400

    Create change_enable_secret.py

---
+++ change_enable_secret.py
@@ -0,0 +1,26 @@
+from netmiko import ConnectHandler
+import csv
+
+ip_list = ['192.1.1.1', '192.1.1.2']
+send_config_commands = ['enable secret secret_here']
+send_write_memory = 'wr mem'
+
+def ssh_begin():
+
+    for devices in ip_list:
+        print(devices)
+        device = ConnectHandler(ip = devices, 
+                                username = 'admin', 
+                                password = 'admin', 
+                                device_type = 'cisco_ios')
+        print(device.find_prompt())
+        print('#' * 80)
+        print(device.send_config_set(send_config_commands))
+        print(device.send_command(send_write_memory))
+        print('#' * 80, '\n')
+
+
+if __name__ == "__main__":
+    ssh_begin()
+
+    
