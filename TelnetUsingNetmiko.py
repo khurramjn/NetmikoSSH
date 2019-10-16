09ece401cfffef6de3356452c96b6470f5313f53
khurramjn <39000712+khurramjn@users.noreply.github.com>
2019-10-15 22:04:31 -0400

    Update TelnetUsingNetmiko.py

--- TelnetUsingNetmiko.py
+++ TelnetUsingNetmiko.py
@@ -4,8 +4,13 @@
 
 from netmiko import ConnectHandler
 
-device = ConnectHandler(ip='10.0.8.18', username='admin', password='Your-Password', device_type='cisco_ios_telnet')
+device = ConnectHandler(
+    ip="10.0.8.18",
+    username="admin",
+    password="Your-Password",
+    device_type="cisco_ios_telnet",
+)
 
 print(device.find_prompt())
-print(device.send_command('sh ip int bri'))
+print(device.send_command("sh ip int bri"))
 device.disconnect()
