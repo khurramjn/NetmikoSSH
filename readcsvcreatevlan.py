3703ea9933447e0e7a5a1c6e3a10c61752255ecf
khurramjn <39000712+khurramjn@users.noreply.github.com>
2019-10-15 22:27:25 -0400

    Update readcsvcreatevlan.py

--- readcsvcreatevlan.py
+++ readcsvcreatevlan.py
@@ -5,19 +5,27 @@ import csv
 iplist = []
 
 # Read the CSV file and Column name is "iplist1"
-with open('ipfile.csv') as fileName:
-    readFile = csv.DictReader(fileName)
+with open("ipfile.csv") as file_name:
+    readFile = csv.DictReader(file_name)
     for read in readFile:
-        iplist.append(read['iplist1'])
+        iplist.append(read["iplist1"])
 
-def createVlan():
+
+def create_vlan():
     for ipAddr in range(len(iplist)):
-        device = ConnectHandler(ip=iplist [ipAddr], username='admin', password='your-password', device_type='cisco_ios')
+        device = ConnectHandler(
+            ip=iplist[ipAddr],
+            username="admin",
+            password="your-password",
+            device_type="cisco_ios",
+        )
         print(device.find_prompt())
-        for x in range(2,5):
-            print('Creating VLAN' + str(x))
-            config_command = ['vlan ' + str(x), 'name Python-Created' + str(x)]
+        for x in range(2, 5):
+            print("Creating VLAN" + str(x))
+            config_command = ["vlan " + str(x), "name Python-Created" + str(x)]
             print(device.send_config_set(config_command))
             print(config_command)
     print(device.disconnect())
-createVlan()
+
+
+create_vlan()
